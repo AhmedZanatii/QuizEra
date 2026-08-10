07fd24f30ff145f4f46425bbad2cda27ba24126b
Ahmed Zanaty <mido.zanaty5@gmail.com>
2026-08-10 14:29:37 +0300

    create entities

--- QuizEra/Program.cs
+++ QuizEra/Program.cs
@@ -1,9 +1,15 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+var connectionString =
+    builder.Configuration.GetConnectionString("Zanatii");
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,6 +18,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
