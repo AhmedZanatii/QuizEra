2b608e94d83c72b5bc4bea59ed185df7843f5b78
Mostafa Mahmoud <mm2453350@gmail.com>
2026-08-14 16:45:58 +0300

    Adding CustomResponse & FileUploader

---
+++ QuizEra.BLL/Common/CustomRespone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizEra.BLL.Common
+{
+    public class CustomResponse<TResult>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public TResult Data { get; set; }
+
+        public static CustomResponse<TResult> Succeded(TResult data, string message = null)
+        {
+            return new CustomResponse<TResult>
+            {
+                Success = true,
+                Message = message ?? "Request successful",
+                Data = data
+            };
+        }
+
+        public static CustomResponse<TResult> Fail(string message)
+        {
+            return new CustomResponse<TResult>
+            {
+                Success = false,
+                Message = message,
+                Data = default
+            };
+        }
+
+    }
+}
---
+++ QuizEra.BLL/Helpers/FileUploader.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+namespace QuizEra.BLL.Helpers
+{
+    public static class FileUploader
+    {
+        public static async Task<string> UploadFile(string folder , IFormFile file)
+        {
+
+
+            var directory = Path.Combine(Directory.GetCurrentDirectory(),"wwwroot/Files", folder);
+            var FileName = Guid.NewGuid() + Path.GetFileName(file.FileName);
+            var filePath = Path.Combine(directory, FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return FileName;
+        }
+        public static void DeleteFile(string folder , string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName))
+            {
+                return;
+            }
+            var directory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files", folder);
+            var filePath = Path.Combine(directory, fileName);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
